29c6645a2dbdc18e7eacd4d1e4b8132cf333ba88
shumaikun0716 <81935614+shumaikun0716@users.noreply.github.com>
2025-02-21 18:36:42 +0900

    Add files via upload

---
+++ get-global-ip.py
@@ -0,0 +1,19 @@
+import requests
+
+def get_global_ip():
+    response = requests.get('https://api.ipify.org?format=json')
+    ip_data = response.json()
+    return ip_data['ip']
+
+global_ip = get_global_ip()
+print(f"グローバルIPv4アドレス: {global_ip}")
+
+import requests
+
+def get_global_ipv6():
+    response = requests.get('https://api64.ipify.org?format=json')
+    ip_data = response.json()
+    return ip_data['ip']
+
+global_ipv6 = get_global_ipv6()
+print(f"グローバルIPv6アドレス: {global_ipv6}")
