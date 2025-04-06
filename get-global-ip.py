3a53f9bb39c63e1c7ed88ae873df36a9779badcd
shumaikun0716 <81935614+shumaikun0716@users.noreply.github.com>
2025-04-06 11:47:23 +0900

    get-global-ip.py を更新

--- get-global-ip.py
+++ get-global-ip.py
@@ -8,8 +8,6 @@ def get_global_ip():
 global_ip = get_global_ip()
 print(f"グローバルIPv4アドレス: {global_ip}")
 
-import requests
-
 def get_global_ipv6():
     response = requests.get('https://api64.ipify.org?format=json')
     ip_data = response.json()
